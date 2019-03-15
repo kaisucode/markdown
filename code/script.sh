a227b1e3a86d2616164c6c2aa5eb11131e506d23
Kaisu <kevin.hsukaihao@gmail.com>
2019-03-14 22:54:58 -0400

    Fix bug; will check if md/pdf file exist first and react appropriately

--- code/script.sh
+++ code/script.sh
@@ -2,5 +2,13 @@
 noExt=$@
 fileExt=".pdf"
 pdfFilename="${noExt: 0:-3}$fileExt"
-mupdf $pdfFilename & . ~/Desktop/2k19/markdown/code/syncPDF.sh & nvim $@; kill $(pgrep mupdf); 
+
+if ! $( [ -f $pdfFilename ] ); then
+	if ! $( [ -f $@ ] ); then
+		echo >> $@
+	fi
+	pandoc -f markdown -t html5 $@ > wkhtmltopdf -o $pdfFilename
+fi
+
+mupdf $pdfFilename & . ~/Scripts/markdown/code/syncPDF.sh & nvim $@; kill $(pgrep mupdf); 
 
